7f58d24bd440436e21c8052e97cc3fc490a6c9ea
matthew-kaye <matthew@brainlabsdigital.com>
2020-09-25 16:22:41 +0100

    add redirect to chat from home

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
+from .views import redirect_view
 
 urlpatterns = [
+    path("", redirect_view),
     path("chat/", include("chat.urls")),
     path("admin/", admin.site.urls),
 ]
